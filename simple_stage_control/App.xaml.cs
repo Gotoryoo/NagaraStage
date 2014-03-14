6cd7ca74a340b31d811b643a44807cf6bc594812
jyoshida <jyoshida@gifu-u.ac.jp>
2014-03-14 15:29:06 +0900

    start

---
+++ simple_stage_control/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace simple_stage_control
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
