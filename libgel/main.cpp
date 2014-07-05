4d2af750fcfd2ec1225a49f442ece578b259f7e2
jyoshida <jyoshida@gifu-u.ac.jp>
2014-07-05 10:20:28 +0900

    test, always pix=225280, isingel=false

--- libgel/main.cpp
+++ libgel/main.cpp
@@ -69,11 +69,20 @@ DLLEXPORT bool __stdcall NagaraStage::isInGelBrightness(
 	int lengthOfSide,
 	int threshold0, int threshold1,
 	double powerOfDifference){        
-		NagaraStage::Image *image = new Image(imageData, width, height);        
-		image->setPowerOfDifference(powerOfDifference);
-		image->setThreshold(threshold1);
-		*brightness0 = image->sumBrightness(startRow, endRow, lengthOfSide);
-		delete image;
+		//NagaraStage::Image *image = new Image(imageData, width, height);        
+		//image->setPowerOfDifference(powerOfDifference);
+		//image->setThreshold(threshold1);
+		//*brightness0 = image->sumBrightness(startRow, endRow, lengthOfSide);
+
+		//cv::Mat mat = cv::Mat(height,width,CV_8U,imageData);
+		//cv::Mat gau = mat.clone();
+		//cv::GaussianBlur(gau, gau, cv::Size(31,31), 0);
+		//cv::subtract(gau, mat, mat);
+		//cv::threshold(mat, mat, threshold1, 1, cv::THRESH_BINARY);
+		
+		cv::Mat mat = cv::Mat::ones(height,width,CV_8U);
+		*brightness0 = cv::countNonZero(mat);
+		//delete image;
 		return false;
 		//return (*brightness0 > threshold0);        
 }
