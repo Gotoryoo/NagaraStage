3cb8ca5fe5a852ff91c0f40cf6b07d945c0cdd60
j.yoshida <jyoshida@gifu-u.ac.jp>
2014-07-30 23:39:07 +0900

    datfile surface-base

--- NagaraStage/Activities/SurfaceLanding.cs
+++ NagaraStage/Activities/SurfaceLanding.cs
@@ -12,149 +12,230 @@ using OpenCvSharp;
 using OpenCvSharp.CPlusPlus;
 
 namespace NagaraStage.Activities {
-    class SurfaceLanding : Activity, IActivity {
-        private static SurfaceLanding instance;
+	class SurfaceLanding : Activity, IActivity {
+		private static SurfaceLanding instance;
 
-        public bool IsActive {
-            get { throw new NotImplementedException(); }
-        }
+		public bool IsActive {
+			get { throw new NotImplementedException(); }
+		}
 
-        public double X {
-            get { return x; }
-            set {
-                x = value;
-                isValidate();
-            }
-        }
+		public double X {
+			get { return x; }
+			set {
+				x = value;
+				isValidate();
+			}
+		}
 
-        public event EventHandler<EventArgs> Started;
-        public event ActivityEventHandler Exited;
-        private ParameterManager parameterManager;
-        private double x;
+		public event EventHandler<EventArgs> Started;
+		public event ActivityEventHandler Exited;
+		private ParameterManager parameterManager;
+		private double x;
 
-        public SurfaceLanding(ParameterManager _paramaterManager)
-            : base(_paramaterManager) {
-            this.parameterManager = _paramaterManager;
-        }
+		public SurfaceLanding(ParameterManager _paramaterManager)
+			: base(_paramaterManager) {
+			this.parameterManager = _paramaterManager;
+		}
 
-        public void Start() {
-            throw new NotImplementedException();
-        }
+		public void Start() {
+			throw new NotImplementedException();
+		}
 
-        public void Abort() {
-            throw new NotImplementedException();
-        }
+		public void Abort() {
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// インスタンスを取得します．
-        /// </summary>
-        /// <returns></returns>
-        public static SurfaceLanding GetInstance(
-            ParameterManager parameterManger = null) {
-            if (instance == null) {
-                instance = new SurfaceLanding(parameterManger);
-            }
-            return instance;
-        }
+		/// <summary>
+		/// インスタンスを取得します．
+		/// </summary>
+		/// <returns></returns>
+		public static SurfaceLanding GetInstance(
+			ParameterManager parameterManger = null) {
+			if (instance == null) {
+				instance = new SurfaceLanding(parameterManger);
+			}
+			return instance;
+		}
 
-        public List<Thread> CreateTask() {
-            List<Thread> taskList = new List<Thread>();
-            taskList.Add(Create(new ThreadStart(task)));
-            return taskList;
-        }
+		public List<Thread> CreateTask() {
+			List<Thread> taskList = new List<Thread>();
+			taskList.Add(Create(new ThreadStart(task)));
+			return taskList;
+		}
 
-        private void task() {
-            MotorControler mc = MotorControler.GetInstance(parameterManager);
-            Camera camera = Camera.GetInstance();
-            Vector3 InitPoint = mc.GetPoint();
-            Vector3 p = new Vector3();
-            int viewcounter = 0;
-            int rowcounter = 0;
-            int colcounter = 0;
+		private void task() {
+			MotorControler mc = MotorControler.GetInstance(parameterManager);
+			Camera camera = Camera.GetInstance();
+			Vector3 InitPoint = mc.GetPoint();
+			Vector3 p = new Vector3();
+			int viewcounter = 0;
+			int rowcounter = 0;
+			int colcounter = 0;
 
-            while (rowcounter < 45) {
-                while (colcounter < 40) {
-                    mc.MovePoint(
-                        InitPoint.X + (parameterManager.ImageLengthX - 0.01) * colcounter,
-                        InitPoint.Y + (parameterManager.ImageLengthY - 0.01) * rowcounter,
-                        InitPoint.Z + 0.01);
-                    mc.Join();
+			while (rowcounter < 2) {
+				while (colcounter < 2) {
+					mc.MovePoint(
+						InitPoint.X + (parameterManager.ImageLengthX - 0.01) * colcounter,
+						InitPoint.Y + (parameterManager.ImageLengthY - 0.01) * rowcounter,
+						InitPoint.Z + 0.01);
+					mc.Join();
 
-                    p = mc.GetPoint();
+					p = mc.GetPoint();
 
-                    bool flag = true;
-                    while (flag) {
-                        mc.MoveDistance(-0.003, VectorId.Z);
-                        mc.Join();
-                        byte[] b = camera.ArrayImage;
-                        Mat src = new Mat(440, 512, MatType.CV_8U, b);
-                        Mat mat = src.Clone();
+					bool flag = true;
+					while (flag) {
+						mc.MoveDistance(-0.003, VectorId.Z);
+						mc.Join();
+						byte[] b = camera.ArrayImage;
+						Mat src = new Mat(440, 512, MatType.CV_8U, b);
+						Mat mat = src.Clone();
 
-                        Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
-                        Mat gau = mat.Clone();
-                        Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
-                        Cv2.Subtract(gau, mat, mat);
-                        Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
-                        int brightness = Cv2.CountNonZero(mat);
+						Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
+						Mat gau = mat.Clone();
+						Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
+						Cv2.Subtract(gau, mat, mat);
+						Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
+						int brightness = Cv2.CountNonZero(mat);
 
-                        viewcounter++;
-                        if (brightness > 10000 || viewcounter > 30) flag = false;
-                    }
+						viewcounter++;
+						if (brightness > 10000 || viewcounter > 30) flag = false;
+					}
 
-                    p = mc.GetPoint();
-                    byte[] bb = camera.ArrayImage;
-                    Mat mat2 = new Mat(440, 512, MatType.CV_8U, bb);
-                    mat2.ImWrite(String.Format(@"c:\img\{0}_{1}_{2}_{3}.bmp", System.DateTime.Now.ToString("yyyyMMdd_HHmmss_fff"),
-                        (int)(p.X * 1000),
-                        (int)(p.Y * 1000),
-                        (int)(p.Z * 1000)));
+					p = mc.GetPoint();
+					byte[] bb = camera.ArrayImage;
+					Mat mat2 = new Mat(440, 512, MatType.CV_8U, bb);
+					mat2.ImWrite(String.Format(@"c:\img\{0}_{1}_{2}_{3}.bmp",
+						System.DateTime.Now.ToString("yyyyMMdd_HHmmss_fff"),
+						(int)(p.X * 1000),
+						(int)(p.Y * 1000),
+						(int)(p.Z * 1000)));
 
-                    viewcounter = 0;
-                    colcounter++;
-                }
-                colcounter = 0;
-                rowcounter++;
-            }
-            /*
-            MotorControler mc = MotorControler.GetInstance(parameterManager);
-            Camera camera = Camera.GetInstance();
-            int viewcounter = 0;
+					string datfileName = string.Format(@"c:\img\{0}_{1}_{2}.dat",
+						System.DateTime.Now.ToString("yyyyMMdd_HHmmss_fff"),
+						(int)(p.X * 1000),
+						(int)(p.Y * 1000));
+					BinaryWriter writer = new BinaryWriter(File.Open(datfileName, FileMode.Create));
 
-            bool flag =true;
-            while (flag) {
-                mc.MoveDistance(-0.003, VectorId.Z);
-                mc.Join();
-                byte[] b = camera.ArrayImage;
-                Mat src = new Mat(440, 512, MatType.CV_8U, b);
-                Mat mat = src.Clone();
+					flag = true;
+					while (flag) {
+						byte[] b = camera.ArrayImage;
+						Mat src = new Mat(440, 512, MatType.CV_8U, b);
+						writer.Write(b);
+						Mat mat = src.Clone();
+						Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
+						Mat gau = mat.Clone();
+						Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
+						Cv2.Subtract(gau, mat, mat);
+						Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
+						int brightness = Cv2.CountNonZero(mat);
+						viewcounter++;
+						if (brightness < 3000) flag = false;
+						mc.MoveDistance(-0.004, VectorId.Z);
+						mc.Join();
+					}
 
-                Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
-                Mat gau = mat.Clone();
-                Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
-                Cv2.Subtract(gau, mat, mat);
-                Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
-                int brightness = Cv2.CountNonZero(mat);
+					writer.Close();
 
-                viewcounter++;
+					viewcounter = 0;
+					colcounter++;
+				}
+				colcounter = 0;
+				rowcounter++;
+			}
 
-                if(brightness > 10000 || viewcounter > 30) flag = false;
-            }
-            byte[] bb = camera.ArrayImage;
-            Mat mat2 = new Mat(440, 512, MatType.CV_8U, bb);
-            Vector3 p = new Vector3();
-            mat2.ImWrite(String.Format(@"c:\img\{0}_{1}_{2}_{3}.bmp",  System.DateTime.Now.ToString("yyyyMMdd_HHmmss_fff"),
-                (int)(p.X * 1000),
-                (int)(p.Y * 1000),
-                (int)(p.Z * 1000)));
-            */
-        }
+			/*
+			MotorControler mc = MotorControler.GetInstance(parameterManager);
+			Camera camera = Camera.GetInstance();
+			Vector3 InitPoint = mc.GetPoint();
+			Vector3 p = new Vector3();
+			int viewcounter = 0;
+			int rowcounter = 0;
+			int colcounter = 0;
 
-        private bool isValidate() {
-            if (x < -100 || x > 100) {
-                throw new ArgumentException("X must be in range from -100 to 100.");
-            }
-            return true;
-        }
+			while (rowcounter < 45) {
+				while (colcounter < 40) {
+					mc.MovePoint(
+						InitPoint.X + (parameterManager.ImageLengthX - 0.01) * colcounter,
+						InitPoint.Y + (parameterManager.ImageLengthY - 0.01) * rowcounter,
+						InitPoint.Z + 0.01);
+					mc.Join();
 
-    }
+					p = mc.GetPoint();
+
+					bool flag = true;
+					while (flag) {
+						mc.MoveDistance(-0.003, VectorId.Z);
+						mc.Join();
+						byte[] b = camera.ArrayImage;
+						Mat src = new Mat(440, 512, MatType.CV_8U, b);
+						Mat mat = src.Clone();
+
+						Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
+						Mat gau = mat.Clone();
+						Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
+						Cv2.Subtract(gau, mat, mat);
+						Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
+						int brightness = Cv2.CountNonZero(mat);
+
+						viewcounter++;
+						if (brightness > 10000 || viewcounter > 30) flag = false;
+					}
+
+					p = mc.GetPoint();
+					byte[] bb = camera.ArrayImage;
+					Mat mat2 = new Mat(440, 512, MatType.CV_8U, bb);
+					mat2.ImWrite(String.Format(@"c:\img\{0}_{1}_{2}_{3}.bmp", System.DateTime.Now.ToString("yyyyMMdd_HHmmss_fff"),
+						(int)(p.X * 1000),
+						(int)(p.Y * 1000),
+						(int)(p.Z * 1000)));
+
+					viewcounter = 0;
+					colcounter++;
+				}
+				colcounter = 0;
+				rowcounter++;
+			}
+			*/
+			/*
+			MotorControler mc = MotorControler.GetInstance(parameterManager);
+			Camera camera = Camera.GetInstance();
+			int viewcounter = 0;
+
+			bool flag =true;
+			while (flag) {
+				mc.MoveDistance(-0.003, VectorId.Z);
+				mc.Join();
+				byte[] b = camera.ArrayImage;
+				Mat src = new Mat(440, 512, MatType.CV_8U, b);
+				Mat mat = src.Clone();
+
+				Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
+				Mat gau = mat.Clone();
+				Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
+				Cv2.Subtract(gau, mat, mat);
+				Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
+				int brightness = Cv2.CountNonZero(mat);
+
+				viewcounter++;
+
+				if(brightness > 10000 || viewcounter > 30) flag = false;
+			}
+			byte[] bb = camera.ArrayImage;
+			Mat mat2 = new Mat(440, 512, MatType.CV_8U, bb);
+			Vector3 p = new Vector3();
+			mat2.ImWrite(String.Format(@"c:\img\{0}_{1}_{2}_{3}.bmp",  System.DateTime.Now.ToString("yyyyMMdd_HHmmss_fff"),
+				(int)(p.X * 1000),
+				(int)(p.Y * 1000),
+				(int)(p.Z * 1000)));
+			*/
+		}
+
+		private bool isValidate() {
+			if (x < -100 || x > 100) {
+				throw new ArgumentException("X must be in range from -100 to 100.");
+			}
+			return true;
+		}
+
+	}
 }
