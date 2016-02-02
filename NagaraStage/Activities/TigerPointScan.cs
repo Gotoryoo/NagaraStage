544384def34eb65a9ae078b0075e699819e1723b
jyoshida <mishina@phys.ed.gifu-u.ac.jp>
2016-02-02 17:22:12 +0900

    tiger point scan, exception

--- NagaraStage/Activities/TigerPointScan.cs
+++ NagaraStage/Activities/TigerPointScan.cs
@@ -84,137 +84,142 @@ namespace NagaraStage.Activities {
         }
 
         private void task() {
-            MotorControler mc = MotorControler.GetInstance(parameterManager);
-            Surface sur = Surface.GetInstance(parameterManager);
-            Camera camera = Camera.GetInstance();
-            Led led = Led.GetInstance();
-            CoordManager cm = new CoordManager(parameterManager);
+            try {
+                MotorControler mc = MotorControler.GetInstance(parameterManager);
+                Surface sur = Surface.GetInstance(parameterManager);
+                Camera camera = Camera.GetInstance();
+                Led led = Led.GetInstance();
+                CoordManager cm = new CoordManager(parameterManager);
 
-            Vector3 InitPoint = mc.GetPoint();
-            Vector3 SurfPoint = mc.GetPoint();
-            Vector3 p = new Vector3();
-            int viewcounter = 0;
-
-
-            //string txtfileName = string.Format(@"{0}\{1}.txt",
-            //    direcotryPath, System.DateTime.Now.ToString("yyyyMMdd_HHmmss_ffff"));
-            //StreamWriter twriter = File.CreateText(txtfileName);
-
-            List<pointscan> PSList = new List<pointscan>();
-
-            var reader = new StreamReader(File.OpenRead(@"C:\test\list.txt"));
-            bool headerflag = true;
+                Vector3 InitPoint = mc.GetPoint();
+                Vector3 SurfPoint = mc.GetPoint();
+                Vector3 p = new Vector3();
+                int viewcounter = 0;
 
 
-            while (!reader.EndOfStream) {
-                var line = reader.ReadLine();
-                string[] delimiter = { " " };
-                var values = line.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+                //string txtfileName = string.Format(@"{0}\{1}.txt",
+                //    direcotryPath, System.DateTime.Now.ToString("yyyyMMdd_HHmmss_ffff"));
+                //StreamWriter twriter = File.CreateText(txtfileName);
 
-                pointscan ps = new pointscan();
-                ps.id = int.Parse(values[1]);
-                ps.stagecoord = new Vector3(
-                    double.Parse(values[12]),
-                    double.Parse(values[13]),
-                    double.Parse(values[14])
-                    );
-                PSList.Add(ps);
-            }
+                List<pointscan> PSList = new List<pointscan>();
+
+                var reader = new StreamReader(File.OpenRead(@"C:\test\list.txt"));
+                bool headerflag = true;
 
 
-            int nshot = 26;
-            byte[] bb = new byte[440 * 512 * nshot];
-            camera.Stop();
+                while (!reader.EndOfStream) {
+                    var line = reader.ReadLine();
+                    string[] delimiter = { " " };
+                    var values = line.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
 
-            for(int pp=0; pp<PSList.Count(); pp++){
-   
-                viewcounter = 0;
-                if (pp % 10 == 0) {
-                    Vector3 surfrecogpoint = PSList[pp].stagecoord;
-                    surfrecogpoint.Z = InitPoint.Z + 0.06;
-                    mc.MoveTo(surfrecogpoint);
-                    mc.Join();
-
-                    bool flag = true;
-                    while (flag) {
-                        mc.MoveDistance(-0.003, VectorId.Z);
-                        mc.Join();
-                        byte[] b = Ipt.CaptureMain();
-                        int brightness;
-
-                        using(Mat src = new Mat(440, 512, MatType.CV_8U, b))
-                        using(Mat mat = src.Clone()){
-                            Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
-                            using (Mat gau = mat.Clone()) {
-                                Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
-                                Cv2.Subtract(gau, mat, mat);
-                                Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
-                                brightness = Cv2.CountNonZero(mat);
-                            }//using gau
-                        }//using src and mat
-
-                        viewcounter++;
-
-                        if (brightness > 15000 || viewcounter > 40) flag = false;
-                    }
-
-                    SurfPoint = mc.GetPoint();
+                    pointscan ps = new pointscan();
+                    ps.id = int.Parse(values[1]);
+                    ps.stagecoord = new Vector3(
+                        double.Parse(values[12]),
+                        double.Parse(values[13]),
+                        double.Parse(values[14])
+                        );
+                    PSList.Add(ps);
                 }
 
-                Vector3 CandPoint = PSList[pp].stagecoord;
-                CandPoint.Z = SurfPoint.Z + CandPoint.Z - 0.044;
 
-                mc.MoveTo(CandPoint);
-                mc.Join();
-
-                camera.Start();
-                led.AdjustLight(parameterManager);
+                int nshot = 26;
+                byte[] bb = new byte[440 * 512 * nshot];
                 camera.Stop();
 
-                p = mc.GetPoint();
-                double prev_z = p.Z;
-                DateTime starttime = System.DateTime.Now;
-                string datfileName = string.Format(@"{0}\{1:00000}_x{2}_y{3}.dat",
-                    direcotryPath,
-                    PSList[pp].id,
-                    (int)(p.X * 1000),
-                    (int)(p.Y * 1000));
-                BinaryWriter writer = new BinaryWriter(File.Open(datfileName, FileMode.Create));
+                for (int pp = 0; pp < PSList.Count(); pp++) {
 
-                string stlog = "";
-                viewcounter = 0;
-                while (viewcounter < nshot) {
-                    mc.MoveDistance(0.003, VectorId.Z);
+                    viewcounter = 0;
+                    if (pp % 1 == 0) {
+                        Vector3 surfrecogpoint = PSList[pp].stagecoord;
+                        surfrecogpoint.Z = InitPoint.Z + 0.06;
+                        mc.MoveTo(surfrecogpoint);
+                        mc.Join();
+
+                        bool flag = true;
+                        while (flag) {
+                            mc.MoveDistance(-0.003, VectorId.Z);
+                            mc.Join();
+                            byte[] b = Ipt.CaptureMain();
+                            int brightness;
+
+                            using (Mat src = new Mat(440, 512, MatType.CV_8U, b))
+                            using (Mat mat = src.Clone()) {
+                                Cv2.GaussianBlur(mat, mat, Cv.Size(3, 3), -1);
+                                using (Mat gau = mat.Clone()) {
+                                    Cv2.GaussianBlur(gau, gau, Cv.Size(31, 31), -1);
+                                    Cv2.Subtract(gau, mat, mat);
+                                    Cv2.Threshold(mat, mat, 10, 1, ThresholdType.Binary);
+                                    brightness = Cv2.CountNonZero(mat);
+                                }//using gau
+                            }//using src and mat
+
+                            viewcounter++;
+
+                            if (brightness > 5000 || viewcounter > 40) flag = false;
+                        }
+
+                        SurfPoint = mc.GetPoint();
+                    }
+
+                    Vector3 CandPoint = PSList[pp].stagecoord;
+                    CandPoint.Z = SurfPoint.Z + CandPoint.Z - 0.044;
+
+                    mc.MoveTo(CandPoint);
                     mc.Join();
-                    byte[] b = Ipt.CaptureMain();
+
+                    camera.Start();
+                    led.AdjustLight(parameterManager);
+                    camera.Stop();
+
                     p = mc.GetPoint();
-                    TimeSpan ts = System.DateTime.Now - starttime;
-                    stlog += String.Format("{0} {1} {2} {3} {4} {5} {6} {7}\n",
-                        pp,
-                        System.DateTime.Now.ToString("HHmmss\\.fff"),
-                        ts.ToString("s\\.fff"),
-                        (p.X * 1000).ToString("0.0"),
-                        (p.Y * 1000).ToString("0.0"),
-                        (p.Z * 1000).ToString("0.0"),
-                        (prev_z * 1000 - p.Z * 1000).ToString("0.0"),
-                        viewcounter);
-                    b.CopyTo(bb, 440 * 512 * viewcounter);
-                    viewcounter++;
+                    double prev_z = p.Z;
+                    DateTime starttime = System.DateTime.Now;
+                    string datfileName = string.Format(@"{0}\{1:00000}_x{2}_y{3}.dat",
+                        direcotryPath,
+                        PSList[pp].id,
+                        (int)(p.X * 1000),
+                        (int)(p.Y * 1000));
+                    BinaryWriter writer = new BinaryWriter(File.Open(datfileName, FileMode.Create));
+
+                    string stlog = "";
+                    viewcounter = 0;
+                    while (viewcounter < nshot) {
+                        mc.MoveDistance(0.003, VectorId.Z);
+                        mc.Join();
+                        byte[] b = Ipt.CaptureMain();
+                        p = mc.GetPoint();
+                        TimeSpan ts = System.DateTime.Now - starttime;
+                        stlog += String.Format("{0} {1} {2} {3} {4} {5} {6} {7}\n",
+                            pp,
+                            System.DateTime.Now.ToString("HHmmss\\.fff"),
+                            ts.ToString("s\\.fff"),
+                            (p.X * 1000).ToString("0.0"),
+                            (p.Y * 1000).ToString("0.0"),
+                            (p.Z * 1000).ToString("0.0"),
+                            (prev_z * 1000 - p.Z * 1000).ToString("0.0"),
+                            viewcounter);
+                        b.CopyTo(bb, 440 * 512 * viewcounter);
+                        viewcounter++;
+                    }
+
+                    //twriter.Write(stlog);
+                    writer.Write(bb);
+                    writer.Flush();
+                    writer.Close();
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+
                 }
 
-                //twriter.Write(stlog);
-                writer.Write(bb);
-                writer.Flush();
-                writer.Close();
+                camera.Start();
+                //twriter.Close();
 
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-                GC.Collect();
-
+            } catch (Exception ex) {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
             }
-
-            camera.Start();
-            //twriter.Close();
         }
 
         private bool isValidate() {
